a41eb7acd25b734a21bb8001d3f154dd2bda3319
Haseeb Khalid <h.khalid@ircmonline.com>
2025-08-04 05:53:27 -0400

    Initial commit

---
+++ cypress/e2e/Home.cy.ts
@@ -0,0 +1,17 @@
+describe('Home Page', () => {
+  it('the h1 contains the correct text', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('[data-test="hero-heading]').contains('Testing Next.js Applications with Cypress')
+    
+   
+  })
+   it.only("the faetures on the homepage are correct",() =>{cy.visit('http://localhost:3000')
+     cy.get("dt").eq(0).contains('4 Courses')
+      cy.get("dt").eq(1).contains('25+ Lessons')
+     cy.get('.mx-auto > .mt-2')
+
+
+   }
+  )
+  
+})
